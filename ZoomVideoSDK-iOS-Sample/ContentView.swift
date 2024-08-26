bb1bfb9bee228a2e5e3d32b7346e8953ca95e172
Shigeki Suwa <shigeki.suwa@oro.com>
2024-08-26 15:54:03 +0900

    init project

---
+++ ZoomVideoSDK-iOS-Sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZoomVideoSDK-iOS-Sample
+//
+//  Created by 諏訪 重貴 on 2024/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZoomVideoSDK-iOS-Sample/ZoomVideoSDK_iOS_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZoomVideoSDK_iOS_SampleApp.swift
+//  ZoomVideoSDK-iOS-Sample
+//
+//  Created by 諏訪 重貴 on 2024/08/26.
+//
+
+import SwiftUI
+
+@main
+struct ZoomVideoSDK_iOS_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
